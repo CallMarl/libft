b5f56e0de2ab0a3e1f2d1d4d1776d177a33bc76a
CallMarl <PaHaGeek@gmail.com>
2017-11-10 19:36:42 +0100

    Update libft rendu

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pprikazs <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 16:59:05 by pprikazs          #+#    #+#             */
-/*   Updated: 2017/11/10 18:07:06 by pprikazs         ###   ########.fr       */
+/*   Updated: 2017/11/10 19:29:19 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,19 @@
 
 size_t		ft_strlcat(char *dest, char *src, size_t size)
 {
-	size_t	len_dest;
-	size_t	len_src;
 	size_t	i;
+	size_t	d_end;
 
 	i = 0;
-	len_dest = (size_t)ft_strlen(dest);//0
-	len_src = (size_t)ft_strlen(src);//1
-	if (size <= len_dest)
-		return (size + len_src);
-	while (--size > 0 && src[i] != '\0')
-	{
-		dest[len_dest + i] = src[i];
+	while (dest[i] && i < size)
 		i++;
-		if (src[i] == 0)
-			dest[len_dest + i - 1] = '\0';
+	d_end = i;
+	while (src[i - d_end] && i < size - 1)
+	{
+		dest[i] = src[i - d_end];
+		i++;
 	}
-	dest[len_dest + i] = '\0';
-	return (len_dest + len_src);
+	if (d_end < size)
+		dest[i] = '\0';
+	return (d_end + ft_strlen(src));
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pprikazs <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 10:48:33 by pprikazs          #+#    #+#             */
-/*   Updated: 2017/11/10 17:53:39 by pprikazs         ###   ########.fr       */
+/*   Updated: 2017/11/10 19:34:54 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,25 @@ int		main(int argc, char **argv)
 	(void)argv;
 
 	char *str = "n\0AA";
-	char buf[0xF00] = "\0AAAAAAAAAAAAAA";
+//	char buf[0xF00] = "\0AAAAAAAAAAAAAA";
 	char buf1[0xF00] = "\0AAAAAAAAAAAAAA";
 	size_t max = 10;
 
-	ft_putnbr(strlcat(buf, str, max));RC;
-	ft_putstr(buf);RC;
-	RC;
-	ft_putnbr(strlcat(buf1, str, max));RC;
-	ft_putstr(buf);RC;
+//	ft_putnbr(strlcat(buf, str, max));RC;
+//	ft_putstr(buf);RC;
+//	RC;
+	ft_putnbr(ft_strlcat(buf1, str, max));RC;
+	ft_putstr(buf1);RC;
 
 	char *str1 = "the cake is a lie !\0I'm hidden lol\r\n";
 	char buff1[0xF00] = "there is no stars in the sky";
-	char buff2[0xF00] = "there is no stars in the sky";
-	size_t max1 = strlen("the cake is a lie !\0I'm hidden lol\r\n") + strlen("there is no stars in the sky");
-
-	ft_putnbr(strlcat(buff2, str1, max1));RC;
-	ft_putstr(buff2);RC;
-	ft_putnbr(ft_strlen(buff2));RC;
-	ft_putnbr(ft_strlcat(buff1, str1, max1));RC;
+//	char buff2[0xF00] = "there is no stars in the sky";
+//	size_t max1 = strlen("the cake is a lie !\0I'm hidden lol\r\n") + strlen("there is no stars in the sky");
+	size_t max2 = 1000;
+//	ft_putnbr(strlcat(buff2, str1, max1));RC;
+//	ft_putstr(buff2);RC;
+//	ft_putnbr(ft_strlen(buff2));RC;
+	ft_putnbr(ft_strlcat(buff1, str1, max2));RC;
 	ft_putstr(buff1);RC;
 	ft_putnbr(ft_strlen(buff1));RC;
 
